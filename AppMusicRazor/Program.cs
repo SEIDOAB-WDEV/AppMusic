1f3a16faa7535df549df4a6615dcf99ff87c035d
Martin <martin_lenart@icloud.com>
2024-01-08 18:21:43 +0100

    final

--- AppMusicRazor/Program.cs
+++ AppMusicRazor/Program.cs
@@ -8,27 +8,7 @@ using Models;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-#region L1.4 Enabling Razor Pages
-//Add a folder called Pages.
-//Copy index.html from wwwroot into Pages
-//rename index.html into index.cshtml
-
-//in index.cshtml change
-//  <title>Simple Razor page</title>
-//  <h1 class="display-5 fw-normal">Simple Razor page</h1>
-//  add @page to the very top (try also without it)
-
-//Adds everything needed for secure RazorPages
 builder.Services.AddRazorPages();
-/*
-//during development this could AFT can be swithced off all the time
-builder.Services.AddRazorPages(o =>
-{
-    o.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
-});
-*/
-#endregion
 
 builder.Services.AddIdentityDbContext();
 builder.Services.AddDefaultIdentity<csUser>(options => {
@@ -64,7 +44,6 @@ else
 
 var app = builder.Build();
 
-#region L1.1 Testing and securing the website
 //using Hsts and https to secure the site
 if (!app.Environment.IsDevelopment())
 {
@@ -75,26 +54,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-#endregion
 
-#region L1.3 Enabling static and default files
-//Create a wwwroot directory.
-//Copy the files from SimpleStatic into wwwroot directory
-
-//Enable static and default files
-app.UseDefaultFiles();
 app.UseStaticFiles();
-
-//try https://localhost:5001/index.html
-//to make this default, simply change app.MapGet("/hello",...) 
-
-#endregion
-
 app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
-#region L1.2 Controlling the hosting environment, L1.4 Enabling Razor Pages
 
 //Map Razorpages into Pages folder
 app.MapRazorPages();
@@ -109,7 +75,6 @@ app.MapGet("/hello", () =>
 
     return $"Hello World!\nASPNETCORE_ENVIRONMENT: {_env}\nMyOwn: {_envMyOwn}";
 });
-#endregion
 
 app.Run();
 
--- DbContext/Migrations/SqlServerDbContext/20240108171511_mi_user.Designer.cs
+++ DbContext/Migrations/SqlServerDbContext/20240108171511_mi_user.Designer.cs
@@ -4,6 +4,7 @@ using DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbContext.Migrations.SqlServerDbContext
 {
     [DbContext(typeof(csMainDbContext.SqlServerDbContext))]
-    partial class SqlServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240108171511_mi_user")]
+    partial class mi_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
